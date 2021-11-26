629e08fee658125689e786ecada4831fdee2534f
tokiya.horikawa <ayikot4423@gmail.com>
2021-11-26 23:26:48 +0900

    not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <main>
         <h1>Learn React Router v6</h1>
         <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,14 @@ ReactDOM.render(
         <Route path="/" element={<App />}>
           <Route path="/expenses" element={<Expemses />} />
           <Route path="/invoices" element={<Invoices />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>

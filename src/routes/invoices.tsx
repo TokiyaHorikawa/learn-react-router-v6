e9e36f4b409a015a4d55897aa6baaa5cb77643a3
tokiya.horikawa <ayikot4423@gmail.com>
2021-11-27 23:08:01 +0900

    NavLink

--- src/routes/invoices.tsx
+++ src/routes/invoices.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
@@ -12,13 +12,19 @@ export default function Invoices() {
         }}
       >
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "black",
+              };
+            }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
